7f3f98bbd483b41ca0d398e81e99f2dd17f8ebc0
Issa Ziada <issa000ziadeh@gmail.com>
2026-02-18 14:15:25 +0200

    Initial Commit

---
+++ CSD LAB 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSD LAB 1
+//
+//  Created by Issa's Mac on 18/02/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
