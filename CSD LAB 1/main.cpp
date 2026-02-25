36120ee98b83c3a1413569cfc145e0ff76062083
Issa Ziada <issa000ziadeh@gmail.com>
2026-02-25 18:07:51 +0200

    added

--- CSD LAB 1/main.cpp
+++ CSD LAB 1/main.cpp
@@ -1,14 +1,65 @@
-//
-//  main.cpp
-//  CSD LAB 1
-//
-//  Created by Issa's Mac on 18/02/2026.
-//
-
+//lab 0 : file contains a small math program
+//Author: Issa
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+using namespace std;
+
+
+//Prototypes
+double sumTwoNum(double num1, double num2);
+
+
+
+//shows the menu
+int main()
+{
+    int choice;
+    
+    do{
+        cout<<"0) Exit"<<endl;
+        cout<<"1) Sum Two Numbers"<<endl;
+        cout<<"2) Calculate Area of Circle"<<endl;
+        cout<<"3) Calculate Circumfrance of Circle"<<endl;
+        cout<<"4) Calculate Price With VAT"<<endl;
+        cout<<"Enter Choice: ";
+        cin>>choice;
+        
+        
+        switch (choice)
+        {
+            case 0://no code needed
+                break;
+            case 1:
+            {
+                cout<<"Sum is "<<sumTwoNum(4,2)<<endl; //todo
+            }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+            {
+                cout<<"Wrong Choice"<<endl;
+                break;
+            }
+        }
+    }
+    
+        while (choice!=0);
+        
+        
+    cout<<"Please come back soon"<<endl;
+    
+    return 0;
+    
+    }
+    
+double sumTwoNum(double num1, double num2)
+{
+    return num1+num2;
 }
+
+
